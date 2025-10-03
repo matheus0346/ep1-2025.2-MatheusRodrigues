9c3a69a85a97d51b1b21c5688ff7a3b76c1b5cbc
Matheus <matheusrodriguespontes346@gmail.com>
2025-10-03 15:41:04 -0300

    Classe Especialidade com seus Getters e Setters

---
+++ src/main/java/com/gestaohospitalar/model/Especialidade.java
@@ -0,0 +1,22 @@
+package src.main.java.com.gestaohospitalar.model;
+
+public class Especialidade {
+    private String nome;
+
+    public Especialidade(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
